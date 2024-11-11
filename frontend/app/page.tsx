7ba4e3b21d034bc81b3aa4e6a4b1d9c47be382b9
Asemanijoshua <joshuaasemani27@gmail.com>
2024-11-11 20:22:35 +0000

    created Home Page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-7xl font-bold">ElitWeb Technologies LTD</h1>
       <p className="text-3xl font-bold">Project will start soon</p>
+      <p className="text-3xl font-bold">Thank You</p>
     </main>
   );
 }
