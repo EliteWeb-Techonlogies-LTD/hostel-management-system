268914e83294110d1d4451000b21197900d8aa05
Afriyie-Anthony <kobbydalton@icloud.com>
2024-11-22 21:41:04 +0000

    initial commit

---
+++ frontend/app/about/page.tsx
@@ -0,0 +1,13 @@
+import MainSection from "@/components/about-components/MainSection";
+import Footer from "@/components/home-components/Footer";
+import Navbar from "@/components/home-components/Navbar";
+
+export default function Book (){
+  return (
+    <>
+    <Navbar/>
+    <MainSection/>
+    <Footer/>
+    </>
+  )
+}
--- frontend/components/home-components/Faq.tsx
+++ frontend/components/home-components/Faq.tsx
@@ -1,9 +1,35 @@
-
-
-export default function Faq () {
+export default function Faq() {
   return (
-   
-    <section></section>
-
-  )
+    <div>
+      <div className="py-4 cursor-pointer">
+        <div className="font-bold space-y-4 mb-10">
+          <div>
+            <p className="border-t-[1px] border-black p-2 flex justify-between text-lg">
+              How to book a Hostel?
+            </p>
+          </div>
+          <div>
+            <p className="border-t-[1px] border-black p-2 flex justify-between text-lg">
+              Are there any hidden fees?
+            </p>
+          </div>
+          <div>
+            <p className="border-t-[1px] border-black p-2 flex justify-between text-lg">
+              Can i cancel my booking?
+            </p>
+          </div>
+          <div>
+            <p className="border-t-[1px] border-black p-2 flex justify-between text-lg">
+              Is breakfast included?
+            </p>
+          </div>
+          <div>
+            <p className="border-y-[1px] border-black p-2 flex justify-between text-lg">
+              How do i contact support?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
