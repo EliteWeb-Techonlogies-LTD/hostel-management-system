25b374835a3ba8701781b3d6be6bd5725d687759
Afriyie-Anthony <kobbydalton@icloud.com>
2024-12-05 22:48:08 +0000

    initi

--- frontend/components/home-components/Navbar.tsx
+++ frontend/components/home-components/Navbar.tsx
@@ -18,8 +18,8 @@ export default function Navbar (){
         </ul>
 
         <div className="flex flex-col lg:flex-row py-8 gap-5">
-          <button className="border-[1px] border-black bg-white py-2 px-5">Sign Up</button>
-          <Link href="/login"><button className="border-[1px] border-black bg-black text-white800 py-2 px-5">Log In</button></Link>
+          <button className="border-[1px] border-black bg-white py-2 px-5 text-[#1A355E] font-semibold">Sign Up</button>
+          <Link href="/login"><button className="border-[1px] border-black bg-[#1A355E] text-white800 py-2 px-5">Log In</button></Link>
         </div>
       </nav>
     </header>
