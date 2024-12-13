cb20d2c600d7ff49384f90acc0773f71024b48b8
Afriyie-Anthony <kobbydalton@icloud.com>
2024-12-13 22:49:44 +0000

    initial

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,10 @@
 
-import Main from '@/components/home-components/Main';
-import Navbar from '../components/home-components/Navbar'
 import Footer from '@/components/home-components/Footer';
 
 
 export default function Home() {
   return (
     <main className="bg-white900">
-     <Navbar/>
-     <Main/>
      <Footer/>
     </main>
   );
